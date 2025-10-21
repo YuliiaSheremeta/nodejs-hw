d05d60e25d6ab6b37a5421ebbcb38083f5981aba
Yuliia_Sheremeta <sheremeyulya1996@gmail.com>
2025-10-21 16:46:04 +0200

    combined index.js file and server.js into a server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
+import { initMongoConnection } from './db/connectMongoDB.js';
 import router from './routes/index.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
@@ -11,30 +15,31 @@ import { errors } from 'celebrate';
 
 const PORT = Number(getEnvVar('PORT','3000'));
 
-export const setupServer = () => {
+const bootstrap = async () => {
 
-    const app = express();
+  await initMongoConnection();
+
+  const app = express();
 
   app.use(logger);
-
   app.use(express.json());
 
   app.use(cors({
   origin: getEnvVar('CLIENT_ORIGIN', 'http://localhost:3000'),
   credentials: true,
 }));
-  app.use(cookieParser());
 
+  app.use(cookieParser());
   app.use(router);
 
   app.use(notFoundHandler);
-
   app.use(errors());
-
   app.use(errorHandler);
 
 
-    app.listen(PORT, () => {
+  app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
 };
+
+bootstrap();
